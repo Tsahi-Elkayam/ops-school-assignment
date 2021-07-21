b1e77ab85075fd0a0dfe7fe3005499ab174f241f
Tsahi-Elkayam <86126183+Tsahi-Elkayam@users.noreply.github.com>
2021-07-21 09:27:20 +0300

    Update exercise6-fix.sh
    changed assignment and pretify args

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -4,7 +4,8 @@
 server1='192.168.100.10'
 server2='192.168.100.11'
 user=vagrant
-#length=$(($#-1))
+num_cp_args=$(($#-1))
+cp_to=${@: -1}
 
 if ["$#" -lt 2]; then 
 	echo "Expected Minimum of 2 Arguments"
@@ -12,16 +13,18 @@ if ["$#" -lt 2]; then
 else:
 	if [$HOSTNAME == $server1]
 	then
-		destination=server2
+		destination=$server2
 	else
-		destination=server1
+		destination=$server1
 	fi
 
 	total=0
-	for i in $(($#-1))
+	i=0
+	
+	for i in $num_cp_args
 	do
 		echo "Copying $i"
-		result="rsync -av i $user@$destination:/${@: -1} | awk 'NR==3'| awk '{print $2}'"
+		result="rsync -av i $user@$destination:/$cp_to | awk 'NR==3'| awk '{print $2}'"
 		total=$(($total + $result))
 	done
 	echo "copied total of $total bytes"
