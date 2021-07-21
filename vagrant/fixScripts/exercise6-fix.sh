c617a8d4931069822bfa11d7e6d0826f084126e1
Tsahi-Elkayam <86126183+Tsahi-Elkayam@users.noreply.github.com>
2021-07-21 13:30:21 +0300

    Update exercise6-fix.sh
    bug fixes

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -6,25 +6,25 @@ server2='192.168.100.11'
 user=vagrant
 num_cp_args=$(($#-1))
 cp_to=${@: -1}
+array=${@:1:$num_cp_args}
 
-if [ "$#" -lt 2 ]; then 
-	echo "Expected Minimum of 2 Arguments"
-	exit 1
+if [ "$#" -lt 2 ]; then
+        echo "Expected Minimum of 2 Arguments"
+        exit 1
 else
-	if [ $HOSTNAME == $server1 ]
-	then
-		destination=$server2
-	else
-		destination=$server1
-	fi
+        if [ $HOSTNAME == $server1 ]
+        then
+                destination=$server2
+        else
+                destination=$server1
+        fi
 
-	total=0
-	
-	for i in 1 .. $num_cp_args
-	do
-		echo "Copying $i"
-		result="rsync -av $i $user@$destination:/$cp_to | awk 'NR==3'| awk '{print $2}'"
-		total=$(($total + $result))
-	done
-	echo "copied total of $total bytes"
+        total=0
+        for i in ${array[@]};
+        do
+                echo "Copying," $i
+                result=$(rsync -chavzP $i $user@$destination:$cp_to | awk 'NR==3'| awk '{print $2}')
+                total=$(($total + $result))
+        done
+        echo "copied total of $total bytes"
 fi
